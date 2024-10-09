72f37df193510eafee85d90e7be63f441824afa1
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2024-10-09 15:38:38 +0800

    remove: unused components

--- libs/components/UserInfo.vue
+++ libs/components/UserInfo.vue
@@ -25,10 +25,7 @@
       </el-collapse-transition>
       </div>
     </el-skeleton>
-    <el-skeleton :loading="!state.chartData.length" :rows="4" anmiated v-if="state.chartData && state.chartExisted">
-      <tmv2-chart chart-height="250px" :chart-rows="state.chartData" :grid="{left: '18%', right: '18%'}" :label-map="state.labelMap" :legend-not-selected="[1]" :y-axis="[{type: 'value', scale: true, minInterval: 1}, {type: 'value', scale: true, minInterval: 1}]" :y-axis-index="[0, 0, 1]" chart-type="line"></tmv2-chart>
-    </el-skeleton>
-    <div v-else class="w-100">
+    <div class="w-100">
       <div class="d-inline me-4" v-for="type in ['followers', 'following']" :key="type">
         <span class="fw-bolder fs-6 me-1">{{ userInfo[type] }}</span>
         <span>{{ t('public.' + type) }}</span>
@@ -66,7 +63,6 @@ const height = computed(() => store.state.height)
 const settings = computed(() => store.state.settings)
 const realMediaPath = computed(() => store.state.realMediaPath)
 const samePath = computed(() => store.state.samePath)
-const updatedCharts = computed(() => store.state.updatedCharts)
 const altitudeDifference = computed(() => store.state.altitudeDifference)
 const userInfo = computed(() => store.state.userInfo)
 
@@ -120,7 +116,6 @@ const getUserInfo = (to: RouteLocationNormalized) => {
       store.dispatch("setCoreValue", {key: 'userExists', value: true})
       store.dispatch("setCoreValue", {key: 'topTweetId', value: response.data.top})
       state.title = (to.name === 'name-display' || to.name === 'name-status') ? userInfo.value.display_name + ' (@' + userInfo.value.name + ') / Twitter Monitor' : "Twitter Monitor"
-      createChart()
     } else {
       Notice(response.message, "error")
       //this.chart.legendName = {'关注者': '关注者', '正在关注': '正在关注', '总推文数': '总推文数'};
@@ -136,39 +131,7 @@ const getUserInfo = (to: RouteLocationNormalized) => {
   })
 }
 
-const createChart = (time: number = 0, refresh: boolean = false) => {
-  request<ApiChartLegacy>(settings.value.basePath + '/api/v3/data/chart/?uid=' + userInfo.value.uid_str + (time > 0 ? '&end=' + time : '') + (refresh ? '&refresh=1' : ''), controllerList.chart).then(response => {
-    if (response.data.length) {
-      state.latestChartTimestamp = Number(response.data.slice(-1)[0].timestamp)
-    }
-    let tmpRows = response.data.map(x => {
-      x.timestamp = (new Date(Number(x.timestamp) * 1000)).toLocaleString(settings.value.language)
-      return x
-    })
-    state.chartData = refresh ? [...state.chartData, ...tmpRows] : tmpRows
-    if (!state.chartData.length && !refresh) {
-      state.chartExisted = false
-    }
-  }).catch(e => {
-    if (controllerList.chart.afterAbortSignal.aborted) {
-      Notice(t("public.loading"), "warning")
-    } else {
-      Notice(t("timeline.scripts.message.failed_to_generate_chart", [String(e)]), "error")
-      setTimeout(() => {
-        createChart(time, refresh)
-      }, 5000)
-    }
-  })
-}
-
 const verifiedStatus = computed(() => VerifiedStatus(userInfo.value.verified))
-
-watch(updatedCharts, () => {
-  if (!updatedCharts.value) {
-    store.dispatch('setCoreValue', {key: 'updatedCharts', value: true})
-    createChart(state.latestChartTimestamp, true)
-  }
-})
 
 onMounted(() => {
   getUserInfo(route)
